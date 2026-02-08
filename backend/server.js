2a9694dc2d51041edfee66a5d7ed363f6b6614e2
Sindhuja Vagilla <vagillasindhuja@gmail.com>
2026-02-08 12:24:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/',(req,res) => {
+  res.send("Volunteer App Backend is Live!");
+});
+
 // Routes
 const volunteerRoutes = require('./routes/volunteerRoutes');
 app.use('/api/volunteer', volunteerRoutes);
